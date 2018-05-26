27901a3d53f5d9a4c4e87a7d9c1fb427d07f8022
pcorneliu <corneliu@rubrikkgroup.com>
2018-05-26 20:52:54 +0300

    Added activities controller with crud action results

---
+++ AcTimer/Controllers/ActivitiesController.cs
@@ -0,0 +1,45 @@
+﻿using AcTimer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace AcTimer.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        // GET: Activities
+        public ActionResult Index()
+        {
+            IEnumerable<Activity> categories = _context.Activities.Include(c => c.Category).Include(u => u.ApplicationUser).ToList();
+            return View(categories);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }        
+
+        [HttpPost]
+        public ActionResult Save(Activity activity)
+        {
+            return View();
+        }
+
+
+    }
+}
